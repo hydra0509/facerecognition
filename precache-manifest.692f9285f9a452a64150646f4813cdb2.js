aa40542aac7b13704c187c0e123ce38017b9a167
Kovács Attila <giga05091@hotmail.com>
2020-08-11 23:20:40 +0200

    Updates

---
+++ precache-manifest.692f9285f9a452a64150646f4813cdb2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b110abbcb45c3b9d2011fe89beb865cf",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "af755e700586aaa7563c",
+    "url": "/facerecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "54dd5b0947be0188a0f6",
+    "url": "/facerecognition/static/css/main.87418c22.chunk.css"
+  },
+  {
+    "revision": "af755e700586aaa7563c",
+    "url": "/facerecognition/static/js/2.dbfa0421.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognition/static/js/2.dbfa0421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54dd5b0947be0188a0f6",
+    "url": "/facerecognition/static/js/main.75d80ffb.chunk.js"
+  },
+  {
+    "revision": "733be2c8cb716d26e320",
+    "url": "/facerecognition/static/js/runtime-main.7c30c7fe.js"
+  },
+  {
+    "revision": "fee6bc469fbbf5acd24244e3ac3c1abc",
+    "url": "/facerecognition/static/media/facelogo.fee6bc46.png"
+  }
+]);
